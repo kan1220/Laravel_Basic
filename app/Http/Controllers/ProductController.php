f18031765b00067450f7d1b81c0f143b778cc45a
古森完 <komorikan@komorikannoMacBook-Air.local>
2024-05-09 20:11:06 +0900

    gitにlaravel_basicをup

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = DB::table('products')->get();
+
+
+        return view('products.index', compact('products'));
+    }
+
+    public function show($id){
+        $product = Product::find($id);
+
+        return view('products.show', compact('product'));
+    }
+}
